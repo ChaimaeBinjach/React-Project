0032a05c8074f34c8b4089add72455c65d00045f
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-05-06 18:26:05 +0200

    Added routing and outlining the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Navbar from './comps/Navbar';
+import AddCustomer from './pages/AddCustomer';
+import Customer from './pages/Customer';
+import Customers from './pages/Customers';
+import EditCustomer from './pages/EditCustomer';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/customers' element={<Customers />} />
+        <Route path='/addcustomer' element={<AddCustomer />} />
+        <Route path='/customer/:id' element={<Customer />} />
+        <Route path='/customer/:id/edit' element={<EditCustomer />} />
+      </Routes>
     </>
   )
 }
