c0be60c5696486730b051e32c394a4d6e42183ee
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-05-06 18:53:10 +0200

    doing Crud functions

---
+++ src/crud.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export async function fetchCustomers() {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/customers`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchCustomer(id) {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/customers/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function createCustomer(newCustomer) {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/customers`, newCustomer);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateCustomer(updatedCustomer) {
+  try {
+    const response = await axios.put(`${API_BASE_URL}/customers/${updatedCustomer.id}`, updatedCustomer);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteCustomer(id) {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/customers/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteAllCustomers() {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/customers`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
