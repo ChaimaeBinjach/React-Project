ffdcd056895248e300019f0215e2c9df501d042d
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-05-06 19:22:16 +0200

    styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import Navbar from './comps/Navbar';
 import AddCustomer from './pages/AddCustomer';
 import Customer from './pages/Customer';
 import Customers from './pages/Customers';
 import EditCustomer from './pages/EditCustomer';
 import LandingPage from './pages/LandingPage';
+import NavBar from './comps/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/customers' element={<Customers />} />
