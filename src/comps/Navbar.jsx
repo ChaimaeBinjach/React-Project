a2a884a585d6b84116107e5a2c8ee083bd888ea8
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-05-07 12:37:41 +0200

    Update navbar

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -1,18 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  const navigate= useNavigate();
   return (
     <Navbar className="navbar" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/" className="brand">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
- 
+          <Nav className="ms-auto">
+            <Nav.Link href="/" className='list_item'>Home</Nav.Link>
+            <Nav.Link href="/customers" className='list_item'>Customers</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <button onClick={() =>navigate(`/addcostumer`)} className="btn btn-success">Add Customer</button>
           </Nav>
         </Navbar.Collapse>
       </Container>
